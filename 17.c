d751d734f71195ed25674b2012f3de7dcc1352d1
ahan10 <90640849+ahan10@users.noreply.github.com>
2022-04-10 01:45:29 -0400

    Add files via upload

---
+++ 17.c
@@ -0,0 +1,25 @@
+/*
+Author : AU2020167 Ahan Bhargava
+Date : 07/01/2021
+Problem Definition : 17. Accept a year from the user and check whether it is leap year or not.
+*/
+#include<stdio.h> 
+int main() 
+{
+    int checkYear;    
+    printf("Enter a year: ");
+    scanf("%d", &checkYear);
+    if( (checkYear % 4 == 0 && checkYear % 100 != 0 ) || (checkYear % 400 == 0) )
+    {
+        printf("%d is a leap year", checkYear);
+    }
+    else
+    {
+        printf("%d is not a leap year", checkYear);
+    }
+    return 0;
+}
+/* Program Output 
+Enter a year: 2020
+2020 is a leap year
+*/
